2ed7eb06c2cdbee9b239357922b847d4c1373217
Shaquille Mandy <shaqmandy@gmail.com>
2022-09-29 15:49:20 -0700

    using array.map to created h1 elements for every name in the this.state

--- src/App.js
+++ src/App.js
@@ -9,34 +9,22 @@ class App extends Component {
 
     // state is always a json object
     this.state = {
-      name : {
-        firstName: 'Shaq',
-        lastName: 'Mandy'
-      },
-      company: 'Amazon',
+      monsters: [
+        { name: 'Linda' },
+        { name: 'Frank'},
+        { name: 'Jacky'},
+        { name: 'Shaquille'},
+      ],
     };
+
   }
 
   render () {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hi {this.state.name.firstName} {this.state.name.lastName} I work at {this.state.company}</p>
-          <button 
-            onClick={() => {
-              this.setState((state, props) => {
-                return {
-                  name : { firstName: 'Shaquille', lastName: 'Mandy'}
-                }
-              }, () => {
-                console.log(this.state)
-              });
-            }}
-          >
-              Change Name
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+            return <h1>{monster.name}</h1>;
+        })}
       </div>
     );
   }
